535aa9786f4dfdb264884b819ced466fe40f05fd
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2024-12-01 13:56:47 +0700

    update dikit

--- TR-Toko-Buku/src/tr/toko/buku/controller/controllerAdmin.java
+++ TR-Toko-Buku/src/tr/toko/buku/controller/controllerAdmin.java
@@ -15,71 +15,87 @@ public class controllerAdmin {
     public controllerAdmin() {
         Koneksi db = new Koneksi();
         db.confiq();
-        this.stm = db.stm; 
+        this.stm = db.stm;
     }
-    
-    public DefaultTableModel tableBuku(){
-        dtm.addColumn("id");
-        dtm.addColumn("Judul Buku");
-        dtm.addColumn("Penulis");
-        dtm.addColumn("Tahun Terbit");
-        dtm.addColumn("Harga");
+
+    public DefaultTableModel Transaction() {
+        DefaultTableModel dtm = new DefaultTableModel();
+        dtm.addColumn("ID");
+        dtm.addColumn("ID pembeli");
+        dtm.addColumn("Buku Dibeli");
+        dtm.addColumn("Total Pembelian");
         return dtm;
     }
-    
-    
-    public boolean addBuku(Book book ){
-         try {
-            this.sql = "INSERT INTO buku (Judul, Author, TahunTerbit, Harga, stok)" + 
-                    "VALUE ('" + book.getTitle() + "' , '" + book.getAuthor()+ "','" + book.getCategory()+ "','" + book.getPrice() + "', '" + book.getStok() + "' )" ;
-            
-            res = stm.executeUpdate(sql);
+
+    public DefaultTableModel tableBuku() {
+        DefaultTableModel dtm = new DefaultTableModel();
+        dtm.addColumn("ID");
+        dtm.addColumn("Judul Buku");
+        dtm.addColumn("Penulis");
+        dtm.addColumn("Genre");
+        dtm.addColumn("Harga");
+        dtm.addColumn("Stok");
+        return dtm;
+    }
+
+    public boolean addBuku(Book book) {
+        try {
+            this.sql = "INSERT INTO buku (Judul, Author, Genre, Harga, Stok) "
+                    + "VALUE ('" + book.getTitle() + "' , '" + book.getAuthor() + "','" + book.getCategory() + "','" + book.getPrice() + "', '" + book.getStok() + "' )";
+
+            this.stm.executeUpdate(sql);
         } catch (Exception e) {
+            System.out.println(e.getMessage());
+            return false;
         }
-        
+
         return true;
     }
-    
-    public void tampilanDataBuku(){
+
+    public void tampilanDataBuku() {
         dtm.getDataVector().removeAllElements();
         dtm.fireTableDataChanged();
-            
+
         try {
-             this.sql = "SELECT * FROM buku";
-             res = stm.executeQuery(sql);
-                        
-            while(res.next()){
-               Object[] obj = new Object [5];
-               obj[0] = res.getString("id");
-               obj[1] = res.getString("Judul");
-               obj[2] = res.getString("Author");
-               obj[3] = res.getString("TahunTerbit");
-               obj[4] = res.getString("Harga");
-               
-               //masukan ke dtm
-               dtm.addRow(obj);
+            this.sql = "SELECT * FROM buku";
+            res = stm.executeQuery(sql);
+
+            while (res.next()) {
+                Object[] obj = new Object[5];
+                obj[0] = res.getString("id");
+                obj[1] = res.getString("Judul");
+                obj[2] = res.getString("Author");
+                obj[3] = res.getString("TahunTerbit");
+                obj[4] = res.getString("Harga");
+
+                //masukan ke dtm
+                dtm.addRow(obj);
             }
-                     
-            
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void tampilanSemuaDataTransaksi() {
+        dtm.getDataVector().removeAllElements();
+        dtm.fireTableDataChanged();
+        try {
+            this.sql = "SELECT * FROM transaksi";
+            res = stm.executeQuery(sql);
+
+            while (res.next()) {
+                Object[] obj = new Object[4];
+                obj[0] = res.getInt("id");
+                obj[1] = res.getInt("idUser");
+                obj[2] = res.getArray("BukuDibeli");
+                obj[3] = res.getDouble("total");
+
+                dtm.addRow(obj);
+            }
+
         } catch (Exception e) {
         }
-    
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-
-    
-
-
-
-
-
 
 }
--- TR-Toko-Buku/src/tr/toko/buku/model/Transaction.java
+++ TR-Toko-Buku/src/tr/toko/buku/model/Transaction.java
@@ -1,15 +1,23 @@
-
 package tr.toko.buku.model;
 
+import java.util.ArrayList;
 
 public class Transaction {
-    
+
     private int idTransaksi;
     private int idPembeli;
-    private int idKeranjang;
+    private ArrayList bukuDibeList = new ArrayList();
 
     public int getIdTransaksi() {
         return idTransaksi;
+    }
+
+    public ArrayList getBukuDibeList() {
+        return bukuDibeList;
+    }
+
+    public void setBukuDibeList(ArrayList bukuDibeList) {
+        this.bukuDibeList = bukuDibeList;
     }
 
     public void setIdTransaksi(int idTransaksi) {
@@ -23,15 +31,15 @@ public class Transaction {
     public void setIdPembeli(int idPembeli) {
         this.idPembeli = idPembeli;
     }
-
-    public int getIdKeranjang() {
-        return idKeranjang;
-    }
-
-    public void setIdKeranjang(int idKeranjang) {
-        this.idKeranjang = idKeranjang;
+    
+    public void addCart(Keranjang krj ){
+        bukuDibeList.add(krj);
     }
     
+    public ArrayList getKeranjang(){
+        return bukuDibeList;
+    }
     
     
+
 }
