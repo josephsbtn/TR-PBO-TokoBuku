0790928f06d3138a904383fe9a31337e69c5682c
josephsbtn <132117803+josephsbtn@users.noreply.github.com>
2024-12-02 14:31:25 +0700

    something

--- TR-Toko-Buku/src/tr/toko/buku/controller/UserController.java
+++ TR-Toko-Buku/src/tr/toko/buku/controller/UserController.java
@@ -78,8 +78,7 @@ public class UserController {
     
     
     //LOGIN
-    public boolean login(String username, String password){
-        
+    public boolean login(String username, String password){     
          User user = new User();
          user.setUsername(username);
          user.setPassword(password);
